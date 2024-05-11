3a99e2388ed0080570b3ef61da1ee85aaed0f2e0
s7k-windows <benotimprotant@mail.ru>
2024-05-11 17:55:53 +0300

    a bug with division fixed

--- SortStation.hpp
+++ SortStation.hpp
@@ -359,8 +359,8 @@ Token enumerate(std::queue< Token > tks) {
                 }
 
                 if (temp2.counttype == UNCOUNTABLE) {
-                    if (!strtemp.empty()) strtemp += "/";
-                    strtemp += "(";
+                    if (strtemp.empty()) strtemp += "1";
+                    strtemp += "/(";
 
                     if (temp2.operand == "*") {
                         read_float(&a, temp2);
